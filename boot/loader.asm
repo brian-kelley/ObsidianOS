433202942f69344a0bdf7c6cbcec9fd9d1ce423f
Brian Kelley <brian.honda11@gmail.com>
2017-04-19 20:10:44 -0500

    Working on 1st stage boot

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,9 +1,21 @@
 ; ObsidianOS bootloader
+; First stage must fit in 446 bytes (image builder will check)
+; First stage's main job is to load 2nd stage loaded at 0x9C000 (624 K)
+; Maybe also show a message on screen.
 
-BITS 16
+; Jobs for 1st stage loader:
+; -Set up stack, segment regs
+; -Get drive geometry (dl has disk # on boot)
+; -Load 2nd stage bootloader at 0x9C000
 
-org 0x7C00
+bits 16
 
-.load: 
+org 0
 
+
+mov ax, 0x7C0
+mov cs, ax
+mov ds, ax
+; set up stack in same segment
+xor 
 
