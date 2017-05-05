6a46977e693bbedc987aa06b8c0f0829a946d6dc
Brian Kelley <brian.honda11@gmail.com>
2017-05-04 19:15:57 -0500

    Testing bootloader

--- boot/boot.asm
+++ boot/boot.asm
@@ -16,45 +16,47 @@
 ; stack will grow towards this data but will never reach it
 ;
 ; Note: initial conditions at boot:
+; org 0x3E: the offset of code within boot sector
 ; ax = # of kernel sectors
 ; bx = first kernel sector (aka start of FS data area)
 ; cx = kernel entry point (absolute memory address)
 ; dl = disk # (possibly used for getting drive geometry)
 ;
-; first, copy whole bootloader from 0x7C00 to 0x9C000
-; then jump to it
+; first, copy whole bootloader from 0x7C00 to 0x8000, then far jump to it
 
 ; temporarily save cx in sp
 mov sp, cx
 mov si, 0x8000
 mov es, si
-mov cx, 223
+mov cx, 256
 ; zero di
 xor di, di
 ; zero ds
 mov ds, di
 mov si, 0x7C00
-; copy 446 bytes (223 words) from ds:0x7C00 (ds:si) to es:0 (es:di)
+; copy boot sector from 0x0:0x7C00 (ds:si) to 0x8000:0 (es:di)
 rep movsw
 ; far jump to resume label in new copy
-jmp 0x4000:resume
+; note: build.py puts an "org 0x3E" at the start of this file, so actual total address is 0x80000 + 0x3E + resume
+jmp 0x8000:resume
 
 resume:
+jmp fillFile
 
 ; restore cx
 mov cx, sp
-mov si, 0x7E0
+; set up ds, ss
+mov si, 0x8200
 mov ds, si
 mov ss, si
 mov bp, 0x1000
 mov sp, bp
 
 ; save ax/bx/cx
-push cx
-push bx
-push ax
+mov [bp + 0], ax
+mov [bp + 2], bx
+mov [bp + 4], cx
 
-mov bp, sp
 ; from now on:
 ; [bp + 0] = # of kernel sectors to read
 ; [bp + 2] = first kernel sector
@@ -81,45 +83,14 @@ shr cl, 6
 mov ah, cl
 mov [0], ax
 
-mov ax, 0xB800
-mov gs, ax
-
-mov dx, 0x0700
-
-xor bx, bx
-
-clearScreen:
-mov [gs:bx], dx
-add bx, 2
-cmp bx, 2000
-jne clearScreen
-
-; print cylinders, heads, sectors/track
-mov al, [0]
-xor bx, bx
-call printNum
-inc bx
-mov al, [2]
-call printNum
-inc bx
-mov al, [4]
-call printNum
-
-hlt
-
-; set video mode to 13h
-mov ah, 0
-mov al, 0x13
-int 0x10
-
-; read the 8k 2nd stage bootloader from FAT filesystem to 0x9C000
-mov ax, 0x9C00
+; read [bp + 0] sectors (starting at [bp + 2]) to 0x500
+mov ax, 0x50
 mov es, ax
 
 ; prepare to read 16 sectors starting at 32 to es:0
-mov ax, 32
+mov ax, [bp + 2]
 mov bx, 0
-mov cx, 16
+mov cx, [bp + 0]
 
 readLoop:
 push ax
@@ -131,29 +102,33 @@ inc ax
 add bx, 512
 loopnz readLoop
 
-;xor bx, bx
-;mov word [es:bx], 0x000F
-;mov word [es:bx + 2], 0x000F
-;mov word [es:bx + 4], 0x000F
+fillFile:
 
-; show some pixels
+; set video mode to 13h
+mov ah, 0
+mov al, 0x13
+int 0x10
+
+; fill with orange (0x2A)
+
 mov ax, 0xA000
-push ds
-mov ds, ax
+mov es, ax
 
-;xor bx, bx
-;mov dx, 8192
-
-drawLoop:
-mov al, [es:bx]
-mov [bx], bh
+xor bx, bx
+fillLoop:
+mov word [es:bx], 0x2A2A
 inc bx
-cmp bx, dx
-jne drawLoop
+inc bx
+cmp bx, 64000 
+jne fillLoop
 
-pop ds
+; read sector into start of video mem to test
+mov ax, 32
+xor bx, bx
+call readLBA
 
-hlt
+stop:
+jmp stop
 
 ; readLBA reads the sector ax (LBA) to es:bx
 ; handles the LBA -> CHS conversion
@@ -185,18 +160,5 @@ readLBA:
   pop dx
   pop ax
   pop bp
-  ret
-
-; print number as ASCII char 33 + al
-; print at column bx in 1st row
-; this can effectively print numbers from 0 to 93
-; preserves all regs
-printNum:
-  push bx
-  add bx, bx
-  add ax, 33
-  mov [gs:bx], al
-  sub ax, 33
-  pop bx
   ret
 
